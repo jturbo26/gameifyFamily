c1c58299a3cfb42d0f5ba43ccf36070234219f13
Jeremy Fry <me@jeremyfry.com>
2017-12-29 16:29:52 -0500

    trying to get the thing to build on heroku

--- express-src/app.js
+++ express-src/app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 app.use(express.static('./dist'))
-app.listen(process.env.PORT || 80, () => console.log('Example app listening on port 80'))
+app.listen(process.env.PORT || 80, () => console.log('Example app listening on port '+(process.env.PORT || 80)))
